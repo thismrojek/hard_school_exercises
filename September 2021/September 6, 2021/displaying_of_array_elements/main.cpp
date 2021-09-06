2d4b4a4bdc3db1cecf642c94e61e9bc210f09a24
Mateusz Rojek <m.rojek@pureinteractive.pl>
2021-09-06 15:53:14 +0200

    Increased numer of elements, added "average" variable

--- September 2021/September 6, 2021/displaying_of_array_elements/main.cpp
+++ September 2021/September 6, 2021/displaying_of_array_elements/main.cpp
@@ -2,20 +2,28 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	int array[5];
+	int array[10];
+	float sumOfElements = 0;
 	
 	cout << "Za moment poprosimy Cie o podanie kilku losowych liczb naturalnych..." << endl;
 	
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 10; i++) {
 		cout << "Podaj liczbe numer " << (i + 1) << ": ";
 		cin >> array[i];
+		sumOfElements += array[i];
+		cout << sumOfElements;
 	}
 	
 	cout << "Teraz wyswietlimy wszystkie elementy..." << endl;
 	
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 10; i++) {
 		cout << "Element " << (i + 1) << ": " << array[i] << endl;
 	}
+	
+	float average = sumOfElements / 10;
+	
+	cout << "Srednia arytmetyczna wszystkich elementow wynosi: " << average;
+	
 	
 	return 0;
 }
