1629d507e816cdced67276ff65da45d985ac1335
Mateusz Rojek <m.rojek@pureinteractive.pl>
2021-09-13 14:42:29 +0200

    Included TASK 1 from 13th September 2021

---
+++ September 2021/September 13, 2021/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+	int array[8] = {1, 2, 4, 6, 8, 11, 15, 19};
+	
+	for (int i = 0; i < 8; i++) {
+		cout << array[i];
+		if (i != 7) {
+			cout << ", ";
+		}	
+	}
+	
+	return 0;
+}
