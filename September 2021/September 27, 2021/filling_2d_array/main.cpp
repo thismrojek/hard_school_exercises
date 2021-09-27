1b1f6b774cfee599021c6851627a703c28ccf087
Mateusz Rojek <m.rojek@pureinteractive.pl>
2021-09-27 15:59:32 +0200

    Display only the first element in a sub-array

--- September 2021/September 27, 2021/filling_2d_array/main.cpp
+++ September 2021/September 27, 2021/filling_2d_array/main.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 int main(int argc, char** argv) {
 	int array[3][2];
-	int sum = 0;
 	
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 2; j++) {
@@ -14,14 +13,12 @@ int main(int argc, char** argv) {
 	}
 	
 	for (int i = 0; i < 3; i++) {
-		if (i % 2 == 0) {
-			for (int j = 0; j < 2; j++) {
-				sum += array[i][j];
+		for (int j = 0; j < 2; j++) {
+			if (j == 0) {
+				cout << "Pierwszy element z podtablicy " << i << " ma wartosc: " << array[i][j] << endl;
 			}
 		}
 	}
-	
-	cout << "Suma wartosci wszystkich elementow z podtablic o parzystych indeksach wynosi " << sum << endl;
 	
 	return 0;
 }
