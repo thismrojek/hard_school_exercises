9382d54d5f9637284d0c8e7c75b3fbfc5ce0b066
Mateusz Rojek <m.rojek@pureinteractive.pl>
2021-10-08 10:17:41 +0200

    Create task1.php

---
+++ October 2021/October 8, 2021/task1.php
@@ -0,0 +1,29 @@
+<?php
+    // Definuję tablicę
+    $myVeryFirstArray = [2, 1, 3, 7, 0];
+
+    // Sprawdzam, czy z formularza "przyszła jakakolwiek wartość - metoda isset()
+    if (isset($_POST['value'])) {
+        $topValue = max($myVeryFirstArray);
+        $valueToCheck = $_POST['value'];
+
+        // Sprawdzam, czy element istnieje w tablicy
+        if (in_array($valueToCheck, $myVeryFirstArray)) {
+            echo "Ta wartość istnieje w tablicy. ";
+        } else {
+            echo "Ta wartość nie istnieje w tablicy. ";
+        }
+
+        // Sprawdzam, czy potęga podanej liczby jest mniejsza od największej wartości w tablicy
+        if ($topValue > pow($valueToCheck, 2)) {
+            echo "Potęga podanej liczby jest mniejsza od największej wartości w tablicy.";
+        }
+    }
+?>
+
+<!-- Formularz do zadania -->
+<form method="POST">
+    <input name="value" placeholder="Podaj wartość" required>
+    <input type="submit" value="Sprawdź!">
+</form>
+
