807fa742b77105ca71fc1011daa440cb91b044de
Mateusz Rojek <m.rojek@pureinteractive.pl>
2021-10-18 15:15:07 +0200

    Checking for number > 0

--- October 2021/October 18, 2021/square_area.cpp
+++ October 2021/October 18, 2021/square_area.cpp
@@ -2,13 +2,22 @@
 using namespace std;
 
 int calculateSquareArea(int sideLength) {
-    int squareArea = sideLength * sideLength;
-    return squareArea;
+    if (sideLength > 0) {
+        int squareArea = sideLength * sideLength;
+        return squareArea;
+    } else {
+        return false;
+    }
 }
 
 int main() {
     int sideLength;
 	cout << "Podaj dlugosc boku kwadratu: ";
     cin >> sideLength;
-    cout << "Pole kwadratu o podanym boku wynosi: " << calculateSquareArea(sideLength);
+    
+    if (calculateSquareArea(sideLength)) {
+        cout << "Pole kwadratu o podanym boku wynosi: " << calculateSquareArea(sideLength);
+    } else {
+        cout << "Podany bok kwadratu nie pozwala na obliczenie pola.";
+    }
 }
